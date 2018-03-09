42dbbfdc172547355bcbdb4cf4196f978f4196d0
DESKTOP-GQKH5RS\Ybr <podonin.developer@gmail.com>
2018-03-09 14:54:05 +0300

    Chapter 31. View realization

---
+++ app/src/main/java/android/bignerdranch/com/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,31 @@
+package android.bignerdranch.com.draganddraw;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ybr on 09.03.2018.
+ */
+
+public class DragAndDrawFragment extends Fragment{
+
+    public static DragAndDrawFragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
+        return v;
+    }
+}
